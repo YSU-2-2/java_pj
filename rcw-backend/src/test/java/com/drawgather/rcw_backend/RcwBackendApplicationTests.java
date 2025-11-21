7c31f5090e22bd6c4c8463e2e13eb8d858aea8b7
jangyunseo2004-oss <jangyunseo2004@gmail.com>
2025-11-21 18:55:02 +0900

    장윤서 ㅋㅋ

---
+++ rcw-backend/src/test/java/com/drawgather/rcw_backend/RcwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawgather.rcw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
